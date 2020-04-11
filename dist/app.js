5da94595707061b8b784606b84386b1705469ed5
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-04-11 12:25:20 -0400

    Local cleanup

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var hello = 'hello';
+
+var doubleHello = function doubleHello() {
+  return console.log('hello');
+};
+
+var goodbye = function goodbye() {
+  return console.log('goodbye');
+};
---
+++ src/es6.js
@@ -0,0 +1,5 @@
+const hello = 'hello';
+
+const doubleHello = () => console.log('hello');
+
+const goodbye = () => console.log('goodbye');
